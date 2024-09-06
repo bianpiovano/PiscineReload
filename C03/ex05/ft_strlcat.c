514e756da752cfbbb5b22c39039d0de2097ae076
bianpiovano <bianpiovano@gmail.com>
2024-09-06 13:54:28 +0200

    Exercise 05 fixed 👌

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bpiovano <bpiovano@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 18:35:34 by bpiovano          #+#    #+#             */
-/*   Updated: 2024/09/04 15:43:51 by bpiovano         ###   ########.fr       */
+/*   Updated: 2024/09/06 13:51:58 by bpiovano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,21 @@ unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size)
 	dest_length = 0;
 	src_length = 0;
 	i = 0;
-	while (dest[dest_length] && src[src_length])
+	while (dest[dest_length])
 	{
 		dest_length++;
+	}
+	while (src[src_length])
+	{
 		src_length++;
 	}
 	if (size <= dest_length)
 	{
 		return (size + src_length);
 	}
-	while (src[i] && (dest_length + 1) < (size - 1))
+	while (src[i] && (dest_length + i) < size)
 	{
-		dest[dest_length + 1] = src[i];
+		dest[dest_length + i] = src[i];
 		i++;
 	}
 	dest[dest_length + i] = '\0';
@@ -44,13 +47,13 @@ unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size)
 
 int	main(void)
 {
-	char			dest[20] = "Hello, ";
-	const char		*src = "world!";
+	char			dest[20] = "Hello";
+	const char		*src = "kitty";
 	unsigned int	size;
 	unsigned int	result;
 
 	size = 20;
-	result = my_strlcat(dest, src, size);
+	result = ft_strlcat(dest, src, size);
 	printf("Result: %u\n", result);
 	printf("Final string: '%s'\n", dest);
 	return (0);
